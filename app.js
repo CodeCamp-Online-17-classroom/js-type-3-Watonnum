fd47eb3a098303b803a443f94b8faaf72e379c18
Watonnum <adamcorktail@gmail.com>
2024-04-30 17:38:43 +0700

    Done

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-// write code here
+let a = undefined;
+let b = " ";
+let c = !b;
+
+let d = Boolean(a);
+let e = Boolean(b);
+let f = Boolean(c);
+
+console.log(d);
+
+console.log(e);
+
+console.log(f);
+
+// เนื่องจาก a ยังไม่ได้ถูก define จึงนับเป็น false
+// เนื่องจาก b เป็น string เปล่า = มีค่า จึงเป็น true
+// เนื่องจาก c มีค่าตรงข้ามกับ b >> !b จึงเป็น false
